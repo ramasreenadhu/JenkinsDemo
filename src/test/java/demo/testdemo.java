97b4bda5ba695ae97a0ac85f38730390f777476c
rama <rama.sreenadhu@macys.com>
2015-08-25 18:30:05 -0700

    added new files

---
+++ src/test/java/demo/testdemo.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class testdemo {
+
+	@Test
+	public void test123() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("asdNeeraj thank for being my roomates");
+
+		System.out.println("77");
+		
+		System.out.println("äa");
+		
+
+	}
+
+}
